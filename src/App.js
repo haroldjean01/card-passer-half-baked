0ea0e6e20ed604c4d052ed9a295022a97efd79da
Harold Jean <haroldjean38@yahoo.com>
2023-01-25 20:41:40 -0500

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import './App.css';
-import initialCards from './cards-data';
 import Player from './components/Player';
 import CardList from './components/CardList';
-import { useState } from 'react';
+import { useContext } from 'react';
 import ExecutePassButton from './components/ExecutePassButton';
+import { GameContext } from './context/GameContext.js';
 
 function App() {
-  const [deck, setDeck] = useState(initialCards);
-  const [playerOneHand, setPlayerOneHand] = useState([]);
-  const [playerTwoHand, setPlayerTwoHand] = useState([]);
-  const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
-  const [to, setTo] = useState(1);
-
-  function findCardIndex(value, suit, cards) {
-    return cards.findIndex((card) => card.value === value && card.suit === suit);
-  }
-
-  function passCard(card) {
-    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
-    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
-
-    // arrays start at zero, but our players start at 1 :shrug:
-    const toHand = playerHands[to - 1] || deck;
-    const fromHand = playerHands[from - 1] || deck;
-
-    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
-    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
-
-    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
-    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
-
-    toHand.push(cardToMove);
-
-    toSetFunction([...toHand]);
-    fromSetFunction([...fromHand]);
-  }
+  const { playerOneHand, playerTwoHand, playerThreeHand, deck, selectedCard } =
+    useContext(GameContext);
 
   return (
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player to={to} player={1} hand={playerOneHand} setFrom={setFrom} setTo={setTo} />
-        <Player to={to} player={2} hand={playerTwoHand} setFrom={setFrom} setTo={setTo} />
-        <Player to={to} player={3} hand={playerThreeHand} setFrom={setFrom} setTo={setTo} />
-        <CardList cards={deck} setFrom={setFrom} player={'deck'} />
+        <Player player={1} hand={playerOneHand} />
+        <Player player={2} hand={playerTwoHand} />
+        <Player player={3} hand={playerThreeHand} />
+        <CardList cards={deck} player={'deck'} />
       </section>
-      <section>
-        {<ExecutePassButton passCard={passCard} setFrom={setFrom} from={from} to={to} />}
-      </section>
+      <section>{selectedCard && <ExecutePassButton />}</section>
     </div>
   );
 }
