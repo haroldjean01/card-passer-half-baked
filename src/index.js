eb23e53b7a0b686f0be17693edbd0762213f3175
Harold Jean <haroldjean38@yahoo.com>
2023-01-25 20:34:33 -0500

    reverted back to start before app broke. Unfornately still broken. Inserted states in GameContext.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GameContext from './context/GameContext.js';
 import { GameProvider } from './context/GameContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
